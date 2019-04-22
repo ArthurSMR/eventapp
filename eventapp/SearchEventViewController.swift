e3405821c82599f3659ae6a0241726dab995db67
Gabi Resende <gabiaresende@gmail.com>
2019-04-22 16:14:37 -0300

    busca

--- eventapp/SearchEventViewController.swift
+++ eventapp/SearchEventViewController.swift
@@ -7,18 +7,70 @@
 //
 
 import UIKit
+extension SearchEventViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
 
 class SearchEventViewController: UIViewController {
-
+    var filteredEvents = [Event]()
+    let searchController = UISearchController(searchResultsController: nil)
    
-    @IBOutlet weak var hoje: UIButton!
+   
+    // MARK: - Private instance methods
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredEvents = events.filter({( event : Event) -> Bool in
+            return event.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+    
+    @IBOutlet weak var hoje: UIButton!  //qdo selecionar mudar de cor
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Buscar Eventos"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
         // Do any additional setup after loading the view.
     }
     
 
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering() {
+            return filteredEvents.count
+        }
+        
+        return events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let event: Event
+        if isFiltering() {
+            event = filteredEvents[indexPath.row]
+        } else {
+            event = events[indexPath.row]
+        }
+        cell.textLabel!.text = event.title
+        cell.detailTextLabel!.text = event.image
+        return cell
+    }
     /*
     // MARK: - Navigation
 
