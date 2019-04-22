0b41e81c5e90725f185d835188ca21293bdce084
Gabi Resende <gabiaresende@gmail.com>
2019-04-22 14:37:07 -0300

    tab bar

---
+++ eventapp/SearchEventViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SearchEventViewController.swift
+//  eventapp
+//
+//  Created by Gabriela Resende on 22/04/19.
+//  Copyright © 2019 Arthur Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class SearchEventViewController: UIViewController {
+
+   
+    @IBOutlet weak var hoje: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
